90bf4d7a6e4c9b42b397873dd788131186b81fe1
Vinamra Mathur <vinamra.mathur@hotmail.co.in>
2021-12-19 20:07:52 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,14 @@
 
+import os
+os.getcwd()
+
+import pandas as pd
+import json
+df = pd.read_json("product (1).json")
+
+with open ("product (1).json",'r', encoding="utf8") as f:
+    x = json.loads(f.read())
+
+pd.DataFrame.from_dict(x[0]).to_csv('Test.csv',index=False)
+
+x[0].keys()
